a38c12b2618306bcec54d5d15462e06de5c2d6f5
Acla <acladominic10@gmail.com>
2025-06-22 17:42:44 +0800

    frontend api

--- fend/src/api/api.js
+++ fend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // ✅ Match exactly with browser origin
+  baseURL: 'https://d1db-149-30-138-246.ngrok-free.app',//'http://127.0.0.1:8000', // ✅ Match exactly with browser origin
   withCredentials: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
